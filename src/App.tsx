4337d107b6cdb8240d6bf61f31315960d1dd30b9
hknp796 <foslux@gmail.com>
2023-03-30 15:05:53 +0530

    basic home page added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import React from "react";
-
+import Home from "./components/Home";
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <Home />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Home.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Image from "../assets/workout.jpeg";
+
+function Home() {
+  return (
+    <div>
+      <div className="flex justify-around items-center h-full">
+        <div className="flex flex-col gap-10">
+          <h3 className="text-red-600 font-bold text-lg">Fitness Club</h3>
+          <p className="text-3xl font-bold">
+            Sweat, Smile and <br />
+            Repeat
+          </p>
+          <button
+            type="button"
+            className="text-red-500 outline outline-black   font-medium rounded-md text-sm px-5 py-2.5 mr-2 mb-2s w-[170px]"
+          >
+            Explore Excercises
+          </button>
+        </div>
+
+        <img src={Image} alt="" className=" w-2/6 rounded-bl-[100px]" />
+      </div>
+      {/* <h1 className="font-bold text-[200px]">Excercise</h1> */}
+    </div>
+  );
+}
+
+export default Home;
