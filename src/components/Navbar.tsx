d9364261b873702fbf2180b348b46a95d615d887
hknp796 <foslux@gmail.com>
2023-04-30 23:27:38 +0530

    icons added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import { FaBeer } from "react-icons/fa";
 function Navbar() {
   return (
     <nav className="flex  absolute gap-10 ml-60 mt-10 text-xl font-semibold">
       <Link to="/">Home</Link>
+      <FaBeer />
       <Link to="/exercises">Exercise</Link>
     </nav>
   );
