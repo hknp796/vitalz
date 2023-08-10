d7c44cbcc6a0038b7019c1073a5e01d6cdaa248e
hknp796 <foslux@gmail.com>
2023-08-10 22:31:38 +0530

    updates

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { FaBeer } from "react-icons/fa";
-function Button({title, icon, ...props}) {
+function Button(props:any) {
   return (
     <div>
       <button
         type="button"
         className="text-black button bg-[#FCA2D5]  font-medium  text-sm px-16 py-7 mr-2 mb-2 rounded-[50px] shadow-[6px_8px_0px_1px_rgba(0,0,0,1)] flex gap-4"
       >
-        {title} <span>{icon && icon}</span>
+        {props.title} <span>{props.icon && props.icon}</span>
       </button>
     </div>
   );
