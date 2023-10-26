f0abed277616473e1a6df724849415023780d46a
hknp796 <foslux@gmail.com>
2023-10-26 21:39:35 +0530

    axios integration with dummy user json

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -15,13 +15,14 @@ export default function Table(props: any) {
             <ul role="list" className="divide-y divide-gray-100">
                 <li className="grid grid-cols-2 justify-between items-center gap-x-6 py-3 bg-[#242526] text-white  rounded-lg mb-1 p-3 cursor-pointer hover:bg-[#1377FF]" onClick={selectClient}>
                     <div className="flex gap-x-4 items-center">
-                        <img className="h-12 w-12 flex-none rounded-full bg-gray-50" src={props.person.imageUrl} alt="" />
+                        <img className="h-12 w-12 flex-none rounded-full bg-gray-50" src={props.person.image} alt="" />
                         <div className="min-w-0 flex-auto">
                             <p className="text-sm font-semibold leading-6 ">{props.person.name}</p>
                         </div>
                     </div>
                     <div className="hidden sm:flex sm:flex-col  sm:justify-center ">
-                        <p className="text-xs leading-5 text-white">Date of Joining : {props.person.dateOfJoining}</p>
+                        <p className="text-xs leading-5 text-white">Date of Joining : {props.person.birthDate
+}</p>
                     </div>
                 </li>
             </ul>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,58 +10,8 @@ import Navbar from "../components/Navbar";
 import SideCard from "../components/SideCard";
 
 function Home() {
-  const peoples = [
-    {
-      name: 'Leslie Alexander',
-      email: 'leslie.alexander@example.com',
-      role: 'Co-Founder / CEO',
-      imageUrl:
-        'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-      dateOfJoining: '2023-01-23T13:23Z',
-    },
-    {
-      name: 'Michael Foster',
-      email: 'michael.foster@example.com',
-      role: 'Co-Founder / CTO',
-      imageUrl:
-        'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-      dateOfJoining: '2023-01-23T13:23Z',
-    },
-    {
-      name: 'Dries Vincent',
-      email: 'dries.vincent@example.com',
-      role: 'Business Relations',
-      imageUrl:
-        'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-      dateOfJoining: '2023-01-23T13:23Z',
-    },
-    {
-      name: 'Lindsay Walton',
-      email: 'lindsay.walton@example.com',
-      role: 'Front-end Developer',
-      imageUrl:
-        'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-      dateOfJoining: '2023-01-23T13:23Z',
-    },
-    {
-      name: 'Courtney Henry',
-      email: 'courtney.henry@example.com',
-      role: 'Designer',
-      imageUrl:
-        'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-      dateOfJoining: '2023-01-23T13:23Z',
-    },
-    {
-      name: 'Tom Cook',
-      email: 'tom.cook@example.com',
-      role: 'Director of Product',
-      imageUrl:
-        'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-      dateOfJoining: '2023-01-23T13:23Z',
-    },
-  ]
+
   let [isOpen, setIsOpen] = useState(false)
-  let [people, setPeople] = useState(peoples)
 
   function closeModal() {
     setIsOpen(true)
@@ -71,13 +21,19 @@ function Home() {
     setIsOpen(false)
   }
 
-  function submitHandler(data: any) {
-    setPeople(current => [...current, data]);
-  }
-
   function details(){
     
   }
+  const [userData, setData] = useState([]);
+  useEffect(() => {
+    axios.get('https://dummyjson.com/users')
+      .then(response => {
+        setData(response.data.users);
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  }, []); 
 
   return (
     <div className="bg-[#19191A] h-[100vh] px-10">
@@ -99,8 +55,8 @@ function Home() {
               </button>
             </div>
           </div>
-          {people.map((person, index) => (
-            <Table key={person.email} person={person}  details={details}/>
+          {userData.map((person, index) => (
+            <Table key={person.id} person={person}  details={details}/>
           ))}
         </div>
       </div>
