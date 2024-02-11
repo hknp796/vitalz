647018d0596af1d8a92d4cd104d0b56e96a35a44
hknp796 <foslux@gmail.com>
2024-02-11 20:08:28 +0530

    doughnut chart added

--- src/components/Doughnut.jsx
+++ src/components/Doughnut.jsx
@@ -27,7 +27,7 @@ const data = {
     datasets: [
         {
             label: '# of Votes',
-            data: [12,12],
+            data: [12, 12],
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
 
@@ -40,13 +40,41 @@ const data = {
         },
     ],
 };
+var value = 75;
+var newData = {
+    labels: [
+        "",
+        ""
+    ],
+    datasets: [
+        {
+            data: [value, 100 - value],
+            backgroundColor: [
+                "#FF6384",
+                "#AAAAAA"
+            ],
+        }],
+    options: {
+        responsive: true,
+        legend: {
+            display: false
+        },
+        cutout: '50%',
+        plugins: {
+            thickness: {
+                thickness: [
+                    [1, 13],
+                ]
+            }
+        }
+    }
+}
 
 function DoughnutChart() {
-  
 
     return (
-        <div className='h-30 w-40'>    
-            <Doughnut data={data} />;
+        <div className='h-30 w-40'>
+            <Doughnut data={newData} />
         </div>
     )
 }
